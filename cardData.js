e269045f92b7ce6893b3734d726fe5e0d32facdb
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2023-08-20 15:21:56 +0600

    Card Data Update :octocat:

--- cardData.js
+++ cardData.js
@@ -1,22 +1,22 @@
 export const cardData = [
   {
-    imageSrc: './assets/cards/card2.webp',
-    alt: 'Card One',
-    title: 'Sengoku Dynasty',
-    discount: '-5%',
-    price: '50.89€'
-  },
-  {
-    imageSrc: './assets/cards/card1.webp',
+    imageSrc: 'https://i.ibb.co/RgR71sv/card2.webp',
     alt: 'Card Two',
-    title: 'The Sims 4 Horse Ranch',
+    title: 'Remnant 2',
     discount: '-25%',
     price: '90.89€'
   },
   {
-    imageSrc: './assets/cards/card3.webp',
+    imageSrc: 'https://i.ibb.co/xmpQQ1P/card1.webp',
+    alt: 'Card One',
+    title: 'The Sims 4 Horse Ranch',
+    discount: '-15%',
+    price: '50.89€'
+  },
+  {
+    imageSrc: 'https://i.ibb.co/QJnRpgH/card3.webp',
     alt: 'Card Three',
-    title: 'Remnant 2',
+    title: 'Sengoku Dynasty ',
     discount: '-40%',
     price: '74.89€'
   }
