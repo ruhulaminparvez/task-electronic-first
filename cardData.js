4f7ca288f87d35958f1a8795a516d7cd776ef353
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2023-08-20 15:05:56 +0600

    Dynamic Data :octocat:

--- cardData.js
+++ cardData.js
@@ -3,21 +3,21 @@ export const cardData = [
     imageSrc: './assets/cards/card2.webp',
     alt: 'Card One',
     title: 'Sengoku Dynasty',
-    discount: '-25%',
-    price: '74.89€'
+    discount: '-5%',
+    price: '50.89€'
   },
   {
     imageSrc: './assets/cards/card1.webp',
     alt: 'Card Two',
     title: 'The Sims 4 Horse Ranch',
     discount: '-25%',
-    price: '74.89€'
+    price: '90.89€'
   },
   {
     imageSrc: './assets/cards/card3.webp',
     alt: 'Card Three',
     title: 'Remnant 2',
-    discount: '-25%',
+    discount: '-40%',
     price: '74.89€'
   }
 ];
